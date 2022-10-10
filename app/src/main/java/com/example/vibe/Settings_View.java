6b85743b165a268e63788716f58b82d5f311a2fc
Fabian Mendoza-Cisneros <47339451+WavyBoy44@users.noreply.github.com>
2022-10-10 19:55:56 -0400

    Merge remote-tracking branch 'origin/14-settings-view' into 14-settings-view

--- app/src/main/java/com/example/vibe/Settings_View.java
+++ app/src/main/java/com/example/vibe/Settings_View.java
@@ -2,13 +2,44 @@ package com.example.vibe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class Settings_View extends AppCompatActivity {
+
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skeleton_view);
+
+        this.getSupportActionBar().setHomeButtonEnabled(true);
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ColorDrawable color = new ColorDrawable(Color.parseColor("#6D37AE"));
+        this.getSupportActionBar().setBackgroundDrawable(color);
+
+        img = (ImageView) findViewById(R.id.imageView);
+        //This is something that will need to be changed later
+        img.setImageResource(R.drawable.ic_launcher_background);
+
+    }
+
+
+    //This method is what makes the back button work
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+
+                // app icon in action bar clicked; goto parent activity.
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
