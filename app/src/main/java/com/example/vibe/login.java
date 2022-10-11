58f8d41a2df7a29e548310ee480080f32e4fe812
“mohammedbenloucif” <mbenlouc@emich.edu>
2022-10-11 10:47:16 -0400

    updated login.java

--- app/src/main/java/com/example/vibe/login.java
+++ app/src/main/java/com/example/vibe/login.java
@@ -19,13 +19,14 @@ public class login extends AppCompatActivity {
 
 
 
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         mCreateBtn = (TextView)findViewById(R.id.createtext);
+        
         mCreateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
