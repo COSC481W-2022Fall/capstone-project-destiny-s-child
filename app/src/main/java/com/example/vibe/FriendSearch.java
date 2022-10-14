7cf0d173812567583d045f9811d66af329c5062d
Mohamad Chaker <moechaker98@gmail.com>
2022-10-14 01:46:26 -0400

    feat: Create Friend Search View
    * add toolbar at the top of the view
    * add a search box and a back arrow to the toolbar
    * add profile picture image view, Full name text view, username text view, and a "chat now" button
    * add functionality to the back button to go back to the chat log view
    
    Issue #15

--- app/src/main/java/com/example/vibe/FriendSearch.java
+++ app/src/main/java/com/example/vibe/FriendSearch.java
@@ -1,8 +1,11 @@
 package com.example.vibe;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.vibe.R;
 
@@ -11,6 +14,31 @@ public class FriendSearch extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friendsearch);
-        getSupportActionBar().hide();
+
+        // toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+
+        //do not show title of app
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        // add back arrow to toolbar
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        }
     }
-}
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle arrow click here
+        if (item.getItemId() == android.R.id.home) {
+            Intent intent = new Intent(FriendSearch.this, ChatLog.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    }
+
