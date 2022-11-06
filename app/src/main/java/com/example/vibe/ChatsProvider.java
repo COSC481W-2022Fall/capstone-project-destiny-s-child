1cc46a7c731ec8812ddff4e1e19739dc23d89148
“mohammedbenloucif” <mbenlouc@emich.edu>
2022-11-06 05:56:48 -0500

    when the user presses the chat now button on the add conversation view, a chats collection is created in the database and stores the id of the chat, as well as the user id and the friend id. if the message the user sends is empty, a toast pop up will say it failed. else the message will me sent to the database.

---
+++ app/src/main/java/com/example/vibe/ChatsProvider.java
@@ -0,0 +1,30 @@
+package com.example.vibe;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+import java.util.ArrayList;
+
+public class ChatsProvider {
+
+    CollectionReference mCollection;
+
+    public ChatsProvider() {
+        mCollection = FirebaseFirestore.getInstance().collection("chats");
+    }
+
+
+    public Query getUserChats(String idUser) {
+        return mCollection.whereArrayContains("ids", idUser);
+    }
+
+
+    public Query getChatsByUser1AndUser2(String idUser1, String idUser2) {
+        ArrayList<String> ids = new ArrayList<>();
+        ids.add(idUser1 + idUser2);
+        ids.add(idUser2 + idUser1);
+        return mCollection.whereIn("id", ids);
+    }
+}
--- app/src/main/java/com/example/vibe/ConversationView.java
+++ app/src/main/java/com/example/vibe/ConversationView.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
@@ -25,6 +26,8 @@ public class ConversationView extends AppCompatActivity {
     TextView username;
     ImageButton send;
     EditText editMessage;
+
+    MessagesProvider mMessageProvider;
 
     FirebaseFirestore db;
     DocumentReference documentReference;
@@ -42,6 +45,8 @@ public class ConversationView extends AppCompatActivity {
 
         //instantiating Firestore Database
         db = FirebaseFirestore.getInstance();
+
+        mMessageProvider = new MessagesProvider();
 
         //getting the current user
         FirebaseUser CurrentUser = FirebaseAuth.getInstance().getCurrentUser();
@@ -64,21 +69,38 @@ public class ConversationView extends AppCompatActivity {
         userId = intent.getStringExtra("userId");
         username.setText(userId);
 
-        //send button handling
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String msg = editMessage.getText().toString();
-                if (!msg.equals("")){
-                    sendMessage(CurrentUser.getUid(), userId, msg);
-                } else {
-                    Toast.makeText(ConversationView.this, "You can't send empty messages" ,Toast.LENGTH_SHORT).show();
-                }
-                //clears the typed out message in the EditText after hitting send
-                editMessage.setText("");
+                createMessage();
             }
         });
 
+
+    }
+
+    private void createMessage() {
+        FirebaseUser CurrentUser = FirebaseAuth.getInstance().getCurrentUser();
+        String newID = userId + CurrentUser.getUid();
+        String textMessage = editMessage.getText().toString();
+        if(!textMessage.equals("")){
+            Message message = new Message();
+            message.setIdChat(newID);
+            message.setIdSender(CurrentUser.getUid());
+            message.setIdReceiver(userId);
+            message.setMessage(textMessage);
+
+            mMessageProvider.create(message).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    editMessage.setText("");
+                    Toast.makeText(ConversationView.this, "message sent", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        } else {
+            Toast.makeText(ConversationView.this, "failed", Toast.LENGTH_SHORT).show();
+        }
 
     }
 
@@ -91,20 +113,6 @@ public class ConversationView extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    //This method is adding the newly created chats into the database to store the messages
-    private void sendMessage(String sender, String receiver, String message){
-        FirebaseUser CurrentUser = FirebaseAuth.getInstance().getCurrentUser();
-        String newChatId = CurrentUser.getUid() + userId;
-        documentReference = db.collection("chats").document(newChatId);
-        Map<String, Object> chat = new HashMap<>();
-        chat.put("sender", sender);
-        chat.put("receiver", receiver);
-        chat.put("message", message);
-
-        //adds user input into Firestore database
-        documentReference.set(chat);
     }
 
 }
